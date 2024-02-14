a1a52176d7d10fdda12527e70513b834556aca2e
depermana12 <deddiapermana97@gmail.com>
2024-02-14 20:51:07 +0700

    initial project

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My store",
+         "items": [
+             {
+                 "name": "Game of Thrones",
+                 "category": "Book",
+                 "price": 30.99
+
+             },
+             {
+                 "name": "Lord of The Ring",
+                 "category": "Book",
+                 "price": 50.99
+             }
+         ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    create_store = {"name": request_data["name"], "items": []}
+    stores.append(create_store)
+    return create_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_list(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "category": request_data["category"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Stores not found"}, 404
